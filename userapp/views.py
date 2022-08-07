145328be9c9b5c10f401d5aee4e23b2eeccd13cd
sergey.Bukov <sergey.Bukov@yandex.ru>
2022-08-07 11:33:56 +0300

    lesson_1_1

--- userapp/views.py
+++ userapp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from rest_framework.viewsets import ModelViewSet
+from .serializers import UserSerializer
+from .models import User
+
+
+class UserViewSet(ModelViewSet):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
